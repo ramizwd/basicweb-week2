ce28554617c099cadf95814c88f597bcfe60ab82
R <ramz9_97@hotmail.com>
2021-11-01 23:17:04 +0200

    added routing and route params

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 'use strict';
+
 const express = require('express');
 const app = express();
 const port = 3000;
+const cats = require('./routes/catRoute');
 
-app.get('/cat', (req, res) => {
-  res.send('From this endpoint you can get cats.')
-});
+app.use('/', cats);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- routes/catRoute.js
+++ routes/catRoute.js
@@ -1,2 +1,28 @@
 'use strict';
 // catRoute
+
+var express = require('express')
+var router = express.Router()
+
+router.get('/cat', (req, res) => {
+    res.send('From this endpoint you can get cats.')
+});
+
+router.post('/cat', (req, res) => {
+    res.send("POST req cats");
+});
+
+router.put('/cat', (req, res) => {
+    res.send("PUT req cats");
+});
+
+router.delete('/cat', (req, res) => {
+    res.send("DELETE req cats");
+});
+
+router.get('/cat/:catId', (req, res) => {
+    console.log('/cat route', req.params);
+    res.send(`From this endpoint you can get cats from catId: ${req.params.catId}`);
+});
+
+module.exports = router;
