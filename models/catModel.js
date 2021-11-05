48ac7dcd74a6ecf9ed79f59b752478517cf040ae
Ramiz <ramz9_97@hotmail.com>
2021-11-05 09:34:39 +0200

    prevent sql injection

--- models/catModel.js
+++ models/catModel.js
@@ -7,7 +7,7 @@ const getCat = async (catId) => {
     // TODO find single cat object from wop_cat table and return it.
     try {
         // TODO: do the LEFT (or INNER) JOIN to get owner's name as ownername (from wop_user table).
-        const [rows] = await promisePool.query(`SELECT * FROM wop_cat WHERE cat_id=${catId}`);
+        const [rows] = await promisePool.execute('SELECT * FROM wop_cat WHERE cat_id = ?', [catId]);
         return rows;
       } catch (e) {
         console.error('error', e.message);
